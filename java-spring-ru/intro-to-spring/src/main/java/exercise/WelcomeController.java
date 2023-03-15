bf97d7c574f082933a90c7b47372d4727b26308b
@hexlet/cli <support@hexlet.io>
2023-03-15 17:07:58 +0300

    submit java-spring-ru/intro-to-spring

---
+++ java-spring-ru/intro-to-spring/src/main/java/exercise/WelcomeController.java
@@ -0,0 +1,30 @@
+package exercise;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+// BEGIN
+// Отмечаем класс, как контроллер
+// Аннотация @RestController включает в себя аннотацию @Controller and @ResponseBody
+// Упрощает создание контроллера
+// При использовании @RestController, использовать аннотацию @ResponseBody нет необходимости
+@RestController
+// Класс контроллера
+public class WelcomeController {
+
+    // Сопоставляем GET запрос на адрес /users с обработчиком
+    @GetMapping("/hello")
+    // Обработчик
+    // Привязываем параметр запроса к параметру метода
+    // и задаём значение по умолчанию
+    public String welcomeUser(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello, %s!", name);
+    }
+
+    @GetMapping("/")
+    public String welcomeSpring() {
+        return "Welcome to Spring";
+    }
+}
+// END
